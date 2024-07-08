26f6fb8b7d9f2f30f89dfe41921114cf03528490
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-08 16:26:08 +0530

    update file

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,9 +1,30 @@
 import React from "react";
 class ComponentA extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "ComponentA",
+    };
+    console.log("ComponentA Constructor");
+
+    this.setState({
+      name: "CA",
+    });
+  }
+  lkfndscfn;
+  static getDerivedStateFromProps() {
+    console.log("ComponentA getDriveredStateByProps");
+    return null;
+  }
+  componentDidMount() {
+    console.log("ComponntA ComponentDidMount");
+  }
   render() {
+    console.log("ComponentA render");
     return (
       <>
-        <h1>ComponentA</h1>
+        <h1>{this.state.name}</h1>
       </>
     );
   }
