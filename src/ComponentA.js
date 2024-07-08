18ec25b321a220efe28e5d2b1ffca85178b0aec4
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-08 13:28:31 +0530

    update file

---
+++ src/ComponentA.js
@@ -0,0 +1,11 @@
+import React from "react";
+class ComponentA extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>ComponentA</h1>
+      </>
+    );
+  }
+}
+export default ComponentA;
