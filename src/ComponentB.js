a1cdb6540aaaaeae4857460542636ce44abcf9ad
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-10 10:40:11 +0530

    update file

--- src/ComponentB.js
+++ src/ComponentB.js
@@ -1,11 +1,32 @@
 import React from "react";
-class ComponentB extends React.Component {
+class ComponentA extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "ComponentA",
+    };
+    console.log("ComponentA Constructor");
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("ComponentA getDriveredStateByProps");
+    return null;
+  }
+  componentDidMount() {
+    console.log("ComponntA ComponentDidMount");
+  }
   render() {
+    console.log("ComponentA render");
+    // this.setState({
+    //   name: "CA",
+    // });
+
     return (
       <>
-        <h1>ComponentB</h1>
+        <h1>{this.state.name}</h1>
       </>
     );
   }
 }
-export default ComponentB;
+export default ComponentA;
