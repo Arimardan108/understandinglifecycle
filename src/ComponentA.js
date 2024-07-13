12dddf36a96ed590cfbab299351be10941b15a5c
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-13 22:36:41 +0530

    Update file

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -6,6 +6,7 @@ class ComponentA extends React.Component {
 
     this.state = {
       name: "ComponentA",
+      data: [],
     };
     console.log("ComponentA Constructor");
   }
@@ -14,11 +15,18 @@ class ComponentA extends React.Component {
     console.log("ComponentA getDriveredStateByProps");
     return null;
   }
+
+  // https://jsonplaceholder.typicode.com/users/
+
   componentDidMount() {
     console.log("ComponntA ComponentDidMount");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }));
   }
   render() {
-    console.log("ComponentA render");
+    console.log(this.state.data);
     // this.setState({
     //   name: "CA",
     // });
@@ -26,9 +34,35 @@ class ComponentA extends React.Component {
     return (
       <>
         <h1>{this.state.name}</h1>
-        <ComponentB />
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ul>
+          {this.state.data.map((item) => (
+            <li>{item.username}</li>
+          ))}
+        </ul>
+
+
+        
       </>
     );
   }
 }
+
 export default ComponentA;
