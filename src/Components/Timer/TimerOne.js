78318cfb333b8f1a759337888ff330f0855ad8ea
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-15 02:33:08 +0530

    Update file

--- src/Components/Timer/TimerOne.js
+++ src/Components/Timer/TimerOne.js
@@ -1,7 +1,11 @@
+import { prettyDOM } from "@testing-library/react";
 import React from "react";
 export default class Timer extends React.Component {
   constructor() {
     super();
+    this.state = {
+      count: 0,
+    };
     console.log("TimerOne Constructer");
   }
   static getDrivedStateFromProps() {
@@ -11,12 +15,25 @@ export default class Timer extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
     return true;
   }
+
+  handleIncrease = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    });
+  };
   render() {
     console.log("TimerOne render");
-    return <h1>Timer</h1>;
+    return (
+      <>
+        <h1>Counter</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.handleIncrease}>Increase</button>
+      </>
+    );
   }
   componentDidMount() {
     console.log("TimerOne componentDidMount");
+    console.log("__________________________");
   }
   getSnapshotBeforeUpdate() {
     console.log("TimeOne getSanpshotBeforeUpdate");
@@ -24,5 +41,6 @@ export default class Timer extends React.Component {
   }
   componentDidUpdate() {
     console.log("TimerOne componentDidUpdate");
+    console.log("___________________________");
   }
 }
