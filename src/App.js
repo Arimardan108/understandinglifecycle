f896c5fb272e2b9ca6ca9c9bbecd1ac66f55014a
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-16 22:10:58 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ComponentA from "./Components/Component/ComponentA";
+import ComponentB from "./Components/Component/ComponentB";
+
 import ErrorBoundry from "./ErrorBoundry";
 class App extends React.Component {
   // constructor() {
@@ -11,6 +13,10 @@ class App extends React.Component {
       <>
         <ErrorBoundry>
           <ComponentA />
+        </ErrorBoundry>
+
+        <ErrorBoundry>
+          <ComponentB />
         </ErrorBoundry>
       </>
     );
