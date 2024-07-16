49a3192e39ee2f423cf317bf578899fe472b4f5d
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-16 14:20:44 +0530

    Update file with new feature

--- src/Components/Component/ComponentA.js
+++ src/Components/Component/ComponentA.js
@@ -20,7 +20,7 @@ class ComponentA extends React.Component {
   componentDidMount() {
     console.log("ComponntA ComponentDidMount");
 
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/user")
       .then((response) => response.json())
       .then((data) => this.setState({ data }));
   }
